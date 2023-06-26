72827b5bed1823411904130623e4bc4860bdb2da
nanya247 <myx4olawale@yahoo.com>
2023-06-26 15:32:29 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
 void print_rev(char *s)
 {
 	int longi = 0;
-	int 0;
+	int o;
 
 	while (*s != '\0')
 	{
@@ -15,7 +15,7 @@ void print_rev(char *s)
 		s++;
 	}
 	s--;
-	for (o = longi; 0 > 0; o--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
