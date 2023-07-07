d361581bbe2615354fd5867c1237221e8d3e178e
nanya247 <myx4olawale@yahoo.com>
2023-07-07 02:43:03 -0700

    0x08. C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,7 +4,7 @@ int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 
 /**
- * is_palindrome - checks id a string is a palindrome
+ * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
  *
  * Return: 1 if it is, 0 it's not
@@ -17,8 +17,7 @@ int is_palindrome(char *s)
 }
 
 /**
- * _strlen_recursion - returns the legth of a string
- * @s: string to calculate the length of
+ * _strlen_recursion - returns the length of
  *
  * Return: length of the string
  */
@@ -35,11 +34,11 @@ int _strlen_recursion(char *s)
  * @i: iterator
  * @len: length of the string
  *
- * Return: 1 if palindrome, 0 if not
+ * Return: 1 id palindrome, 0 if not
  */
 int check_pal(char *s, int i, int len)
 {
-	if (*s(s + i) != *(s + len - 1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if (*s2 != '\0' && s1 == '*')
+		if (*s2 != '\0' && *s1 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
@@ -22,7 +22,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	else if (*s1 == s2)
+	else if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
